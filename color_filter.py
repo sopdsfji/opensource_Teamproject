970a52935a3a39122560f190a013374a13ccf8fe
j0709y <130530915+j0709y@users.noreply.github.com>
2023-12-15 14:51:48 +0900

    Add color_filter.py

---
+++ color_filter.py
@@ -0,0 +1,15 @@
+def color_filter(image):
+    hls = cv2.cvtColor(image, cv2.COLOR_BGR2HLS)
+
+    lower = np.array([20, 150, 20])
+    upper = np.array([255, 255, 255])
+
+    yellow_lower = np.array([0, 85, 81])
+    yellow_upper = np.array([190, 255, 255])
+
+    yellow_mask = cv2.inRange(hls, yellow_lower, yellow_upper)
+    white_mask = cv2.inRange(hls, lower, upper)
+    mask = cv2.bitwise_or(yellow_mask, white_mask)
+    masked = cv2.bitwise_and(image, image, mask = mask)
+
+    return masked
