7620fce60cf8823e312f99322c011f1af5f8c4ec
j0709y <130530915+j0709y@users.noreply.github.com>
2023-12-15 14:55:22 +0900

    Add ROI.py

---
+++ ROI.py
@@ -0,0 +1,19 @@
+def roi(image):
+    x = int(image.shape[1])
+    y = int(image.shape[0])
+
+    _shape = np.array(
+        [[int(0.1*x), int(y)], [int(0.1*x), int(0.1*y)], [int(0.4*x), int(0.1*y)], [int(0.4*x), int(y)], [int(0.7*x), int(y)], [int(0.7*x), int(0.1*y)],[int(0.9*x), int(0.1*y)], [int(0.9*x), int(y)], [int(0.2*x), int(y)]])
+
+    mask = np.zeros_like(image)
+
+    if len(image.shape) > 2:
+        channel_count = image.shape[2]
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    cv2.fillPoly(mask, np.int32([_shape]), ignore_mask_color)
+    masked_image = cv2.bitwise_and(image, mask)
+
+    return masked_image
